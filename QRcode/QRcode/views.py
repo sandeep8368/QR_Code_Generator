dbf1b0a9936932cb6d67199f13ca49fdfba37db6
Sandeep Kumar <156645038+sandeep8368@users.noreply.github.com>
2024-04-04 10:00:04 +0530

    Add files via upload
    This is a QR_code_generator project .
    The tech are uses - Python ,Django ,Html  ,Css.

---
+++ QRcode/QRcode/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from qrcode import *
+data = None
+
+def home(request):
+    global data
+    if request.method =='POST':
+        data = request.POST['data']
+        img = make(data)
+        img.save("static/image/test.png")
+    else:
+        pass
+    return render(request,"home.html",{'data':data})
